41b712b25aae29a9051dc8075809d9a1b14ee4ab
Zeno Zeng <zenoofzeng@gmail.com>
2017-07-07 12:32:48 +0800

    glibc x86_64

--- scripts/rootfs.sh
+++ scripts/rootfs.sh
@@ -7,7 +7,7 @@ rm -rf rootfs.tar.xz
 mkdir -p rootfs
 tar xf busybox/glibc/busybox.tar.xz -C rootfs
 
-wget https://www.archlinux.org/packages/core/i686/glibc/download/ -O glibc.pkg.tar.xz
+wget https://www.archlinux.org/packages/core/x86_64/glibc/download/ -O glibc.pkg.tar.xz
 mkdir -p glibc
 tar xf glibc.pkg.tar.xz -C glibc
 cp glibc/usr/lib/libc-2.25.so rootfs/lib/libc.so.6
