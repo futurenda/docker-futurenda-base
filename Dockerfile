cd3d00e76a3f284ee5fe118a59958d4d02a0f918
Zeno Zeng <zenoofzeng@gmail.com>
2017-07-07 12:29:37 +0800

    rootfs

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
-FROM busybox:1.26.2-glibc
+FROM scratch
+ADD rootfs.tar.xz /
 
 ENV TINI_VERSION v0.15.0
 ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
 RUN chmod +x /tini
+
 ENTRYPOINT ["/tini", "--"]
+CMD ["sh"]
---
+++ scripts/rootfs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+rm -rf rootfs
+rm -rf glibc
+rm -rf rootfs.tar.xz
+
+mkdir -p rootfs
+tar xf busybox/glibc/busybox.tar.xz -C rootfs
+
+wget https://www.archlinux.org/packages/core/i686/glibc/download/ -O glibc.pkg.tar.xz
+mkdir -p glibc
+tar xf glibc.pkg.tar.xz -C glibc
+cp glibc/usr/lib/libc-2.25.so rootfs/lib/libc.so.6
+
+cd rootfs
+tar -Jcvf ../rootfs.tar.xz .
+cd ..
